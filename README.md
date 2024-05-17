430fd93b5937222e18f95c002040355f50ee7115
hajin <hyunkyoung04@naver.com>
2024-05-17 11:29:46 +0900

    push

--- README.md
+++ README.md
@@ -1,36 +1,64 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Prisma
+    Prisma = ORM(Object Relation Mapper)
+    -> 정해진 형식에 맞춰 작성하면 Prisma에서 SQL쿼리로 변경하여 마이그레이션 등을 수행해준다
 
-## Getting Started
+    링크
+    https://www.prisma.io/
 
-First, run the development server:
+# PlantScale
+    PlantScale = Serverless MySql Database
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+    장점
+        1. 서버관리를 할 필요가 없음
+        2. 사용하지 않은 부분에 대해서는 서버비용을 지불하지 않아도 됨
+        3. 확장을 도와줄 기능 보유
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+    링크
+    https://planetscale.com/
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+# Vercel
+    Vercel - Next.js 개발 팀에서 만든 호스팅 사이트
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+    링크
+    https://vercel.com/
 
-## Learn More
+# Cloudflare
+    Cloudflare Image - 유저의 사진 업로드를 관리
 
-To learn more about Next.js, take a look at the following resources:
+    장점
+        1.  가격이 저렴
+        2.  이미지에 대한 최적화가 잘 되어있음
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+    Cloudflare Stream - 동영상 업로드를 관리, 라이브 스트림도 가능 
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+    장점
+        1. 작업에 들어가는 시간이 짧음
 
-## Deploy on Vercel
+    링크
+    https://www.cloudflare.com/
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+# 터미널 초기 설정작업
+    npx create-next-app@latest
+        - NextJs 앱 생성기 -> NextJs 앱의 초기 설정을 해주는 명령어
+        Tailwind 설치 및 Typescript 설정등을 도와줌
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+    ✔ What is your project named? … carrot-market-reloaded
+    ✔ Would you like to use TypeScript? …  Yes
+    ✔ Would you like to use ESLint? … Yes
+    ✔ Would you like to use Tailwind CSS? …  Yes
+    ✔ Would you like to use `src/` directory? … No 
+    ✔ Would you like to use App Router? (recommended) …  Yes
+    ✔ Would you like to customize the default import alias (@/*)? … No 
+   
+
+# Tailwind CSS
+    Tailwind CSS = Utility-First CSS 프레임워크(작은 class 이름들로 가득한 CSS파일)
+
+    Tailwind CSS IntelliSense를 vscode 확장프로그램 설치해 Tailwind CSS 홈페이지에 들어거서 찾는 시간을 줄일수 있다.
+    
+    * 자동완성이 나오지 않는 경우도 있는데 이때는 Ctrl + Space 버튼을 누르면 자동완성이 나옴 *
+
+    - Tailwind CSS는 모바일 우선으로 적용됨으로 모든 class들이 소형기기에 최적화 되어있음
+
+    링크
+    https://tailwindcss.com/
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,91 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    // <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+    //     <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600">
+    //       <div className="flex justify-between items-center">
+    //         <div className="flex flex-col">
+    //           <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">In Transit</span>
+    //           <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
+    //         </div>
+    //         <div className="size-12 rounded-full bg-orange-400"/>
+    //       </div>
+    //       <div className="my-2 flex items-center gap-2">
+    //         <span className="px-2.5 py-1.5 bg-green-400 rounded-full text-white font-medium uppercase text-xs transition hover:bg-green-500 hover:scale-125">Today</span>
+    //         <span className="dark:text-gray-100">9:30 ~ 10:30</span>
+    //       </div>
+    //       <div className="relative">
+    //         <div className="bg-gray-200 w-full h-2 rounded-full absolute"/>
+    //         <div className="bg-green-400 w-2/3 h-2 rounded-full absolute" />
+    //       </div>
+    //       <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
+    //         <span>Expected</span>
+    //         <span>Sorting center</span>
+    //         <span>In transit</span>
+    //         <span className="text-gray-300 dark:text-gray-500">Delivered</span>
+    //       </div>
+    //     </div>
+    // </main>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    // 반응형,peer,그라디언트 넣기 예제
+    // <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+    //     <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
+    //         <input className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-purple-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:bg-red-100 invalid:focus:ring-red-500 peer" type="email" required placeholder="Email address"/>
+    //         <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required</span>
+    //         <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10 bg-black peer-invalid:bg-red-100">Log in</button>
+    //     </div>
+    // </main>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    //  선택자로 구분 예제
+    //  <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+    //     <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
+    //         <input className="w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent focus:ring-purple-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:bg-red-100 invalid:focus:ring-red-500 peer" type="text" required placeholder="Email address"/>
+    //         <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required</span>
+    //         <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10 bg-black peer-invalid:bg-red-100">Log in</button>
+    //     </div>
+    // </main>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-4">
+         {["Nico","Me","You", "Yourslef"].map((person,index)=>
+         <div key={index} className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl">
+          <div className="size-10 bg-blue-400 rounded-full" />
+          <span className="text-lg font-medium">{person}</span>
+          <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+            <span>{index}</span>
+          </div>
+         </div>)}
       </div>
     </main>
   );
 }
+// dark modifier는 기본 컴퓨터 화면모드에 따라 바뀐다 
+
+
+/**
+ * 
+  .ring {
+      --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+      --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(3px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+      box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
+  }
+ * 
+ * 
+ * ring class는 두개의 변수를 생성(--tw-ring-offset-shadow , --tw-ring-shadow )
+ * 기본적으로 정해진 속성을 쓰는 경우도 있지만 ring의 경우 세세하게 지정해서 만들어낼 수 있다
+ * 
+ * 예를 들면 
+  .bg-black {
+      --tw-bg-opacity: 1;
+      background-color: rgb(0 0 0 / var(--tw-bg-opacity));
+  }
+ * 
+   --tw-bg-opacity의 경우 우리가 지정해서 만들어낼 수 있다
+   bg-opacity-50를 className에 같이 써주면 지정된다
+
+
+
+   그라데이션 
+   bg-gradient-to-tr from-cyan-500 via-yellow-200 to-purple-400
+ * 
+ */
